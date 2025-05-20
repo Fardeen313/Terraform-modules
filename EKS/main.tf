d0b552aab93b2435cc5b38861871671c96694006
FardeenAttar <fardeenisa@gmail.com>
2025-05-20 20:54:28 +0530

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -113,7 +113,7 @@ provider "aws" {
  # data source 
  data "aws_vpc" "main" {
   tags = {
-    Name = "mario_vpc"  # Specify the name of your existing VPC
+    Name = "Jumphost_vpc"  # Specify the name of your existing VPC
   }
 }
 
