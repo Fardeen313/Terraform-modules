893f342cb3ef5d397e3a3d693a1d3b36386f9b1f
FardeenAttar <fardeenisa@gmail.com>
2025-05-20 20:31:17 +0530

    Update variables.tf

--- EKS/variables.tf
+++ EKS/variables.tf
@@ -1,195 +1,72 @@
-provider "aws" {
-  region = "us-east-1"  # Specify your desired region
+variable "region" {
+  description = "AWS region"
+  type = string
+  default = "us-east-1"
 }
 
- #Creating IAM role for EKS
-  resource "aws_iam_role" "master" {
-    name = "veera-eks-master1"
-
-    assume_role_policy = jsonencode({
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Effect": "Allow",
-          "Principal": {
-            "Service": "eks.amazonaws.com"
-          },
-          "Action": "sts:AssumeRole"
-        }
-      ]
-    })
-  }
-
-  resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-    role       = aws_iam_role.master.name
-  }
-
-  resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-    role       = aws_iam_role.master.name
-  }
-
-  resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-    role       = aws_iam_role.master.name
-  }
-
-  resource "aws_iam_role" "worker" {
-    name = "veera-eks-worker1"
-
-    assume_role_policy = jsonencode({
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Effect": "Allow",
-          "Principal": {
-            "Service": "ec2.amazonaws.com"
-          },
-          "Action": "sts:AssumeRole"
-        }
-      ]
-    })
-  }
-
-  resource "aws_iam_policy" "autoscaler" {
-    name = "veera-eks-autoscaler-policy1"
-    policy = jsonencode({
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Action": [
-            "autoscaling:DescribeAutoScalingGroups",
-            "autoscaling:DescribeAutoScalingInstances",
-            "autoscaling:DescribeTags",
-            "autoscaling:DescribeLaunchConfigurations",
-            "autoscaling:SetDesiredCapacity",
-            "autoscaling:TerminateInstanceInAutoScalingGroup",
-            "ec2:DescribeLaunchTemplateVersions"
-          ],
-          "Effect": "Allow",
-          "Resource": "*"
-        }
-      ]
-    })
-  }
-
-  resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-    role       = aws_iam_role.worker.name
-  }
-
-  resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-    role       = aws_iam_role.worker.name
-  }
-
-  resource "aws_iam_role_policy_attachment" "AmazonSSMManagedInstanceCore" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-    role       = aws_iam_role.worker.name
-  }
-
-  resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-    role       = aws_iam_role.worker.name
-  }
-
-  resource "aws_iam_role_policy_attachment" "s3" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-    role       = aws_iam_role.worker.name
-  }
-
-  resource "aws_iam_role_policy_attachment" "autoscaler" {
-    policy_arn = aws_iam_policy.autoscaler.arn
-    role       = aws_iam_role.worker.name
-  }
-
-  resource "aws_iam_instance_profile" "worker" {
-    depends_on = [aws_iam_role.worker]
-    name       = "veera-eks-worker-new-profile1"
-    role       = aws_iam_role.worker.name
-  }
- 
- # data source 
- data "aws_vpc" "main" {
-  tags = {
-    Name = "mario_vpc"  # Specify the name of your existing VPC
-  }
+variable "vpc-name" {
+  description = "VPC Name for our Jumphost server"
+  type = string
+  default = "Jumphost-vpc"
 }
 
-data "aws_subnet" "subnet-1" {
- vpc_id = data.aws_vpc.main.id
- filter {
-    name = "tag:Name"
-    values = ["Jumphost-subnet1"]
- }
+variable "igw-name" {
+  description = "Internet Gate Way Name for our Jumphost server"
+  type = string
+  default = "Jumphost-igw"
 }
 
-data "aws_subnet" "subnet-2" {
- vpc_id = data.aws_vpc.main.id
- filter {
-    name = "tag:Name"
-    values = ["Jumphost-subnet2"]
- }
-}
-data "aws_security_group" "selected" {
-  vpc_id = data.aws_vpc.main.id
-  filter {
-    name = "tag:Name"
-    values = ["Jumphost-sg"]
- }
+variable "subnet-name1" {
+  description = "Subnet Name for our Jumphost server"
+  type = string
+  default = "Jumphost-subnet1"
 }
 
- #Creating EKS Cluster
-  resource "aws_eks_cluster" "eks" {
-    name     = "project-eks"
-    role_arn = aws_iam_role.master.arn
+variable "subnet-name2" {
+  description = "Subnet Name for our Jumphost server"
+  type = string
+  default = "Jumphost-subnet2"
+}
+variable "rt-name" {
+  description = "Route Table Name for our Jumphost server"
+  type = string
+  default = "Jumphost-rt"
+}
 
-    vpc_config {
-      subnet_ids = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
-    }
+variable "sg-name" {
+  description = "Security Group for our Jumphost server"
+  type = string
+  default = "Jumphost-sg"
+}
 
-    tags = {
-      "Name" = "MyEKS"
-    }
 
-    depends_on = [
-      aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
-      aws_iam_role_policy_attachment.AmazonEKSServicePolicy,
-      aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
-    ]
-  }
- resource "aws_eks_node_group" "node-grp" {
-    cluster_name    = aws_eks_cluster.eks.name
-    node_group_name = "project-node-group"
-    node_role_arn   = aws_iam_role.worker.arn
-    subnet_ids      = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
-    capacity_type   = "ON_DEMAND"
-    disk_size       = 20
-    instance_types  = ["t2.small"]
+variable "iam-role" {
+  description = "IAM Role for the Jumphost Server"
+  type = string
+  default = "Jumphost-iam-role1"
+}
 
-    remote_access {
-      ec2_ssh_key               = "us-east-1"
-      source_security_group_ids = [data.aws_security_group.selected.id]
-    }
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0bb84b8ffd87024d8" // Replace with the latest AMI ID for your region
+}
 
-    labels = {
-      env = "dev"
-    }
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.medium"
+}
 
-    scaling_config {
-      desired_size = 2
-      max_size     = 4
-      min_size     = 1
-    }
+variable "key_name" {
+  description = "EC2 keypair"
+  type        = string
+  default     = "us-east-1"
+}
 
-    update_config {
-      max_unavailable = 1
-    }
-
-    depends_on = [
-      aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-      aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-      aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-    ]
-  }
+variable "instance_name" {
+  description = "EC2 Instance name for the jumphost server"
+  type        = string
+  default     = "Jumphost-server"
+}
+#
